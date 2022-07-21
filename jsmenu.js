ce3f70061b47031c58c3be2304c306c602c8c69f
= <=>
2022-07-21 19:41:25 +0200

    Commit Realizado

---
+++ jsmenu.js
@@ -0,0 +1,29 @@
+
+var lista = ["casa", "user","mensaje", "ayuda","ajuste"];
+
+var ico = document.getElementsByTagName("img");
+
+for(var i=0; i<ico.length; i++){
+    ico[i].addEventListener("mouseover", entrar);
+    ico[i].addEventListener("mouseout", salir);
+}
+var colores = ["#f53b57","#c4ff33","#33ffca", "#3386ff", "#9c33ff"];
+var pop = ["home", "user", "mensaje","ayuda","ajuste"];
+
+function entrar(e){
+    var boton_pulsado = e.target.id;
+    document.getElementById(boton_pulsado).style.background = colores[boton_pulsado];
+    document.getElementById(pop[boton_pulsado]).style.visibility = "visible";
+    document.body.style.background = colores[boton_pulsado];
+    
+}
+function salir(e){
+    var boton_pulsado2 = e.target.id;
+    document.getElementById(boton_pulsado2).style.background = "white";
+    document.getElementById(pop[boton_pulsado2]).style.visibility = "hidden";
+    document.body.style.background = "white";
+}
+
+window.onload = function(){
+        alert("Acerca el cursor sobre cualquier icono");
+}
